5e89451d0d35844a72be51b54eedff775068027e
Md Mahbubur Rahman <mahbubur.r.aaman@gmail.com>
2025-09-16 07:12:36 +0600

    refactor comments for clarity

--- scripts/flash.sh
+++ scripts/flash.sh
@@ -6,6 +6,7 @@
 #           - Commercial license required for closed-source use
 # Author  : Md Mahbubur Rahman
 # URL     : <https://m-a-h-b-u-b.github.io>
-# GitHub  : <https://github.com/m-a-h-b-u-b/M2-Bootloader-RUST>
+# GitHub  : <https://github.com/m-a-h-b-u-b/M2-Bootloader-Rust>
+
 probe-rs-cli download target/thumbv7em-none-eabihf/debug/bootloader --chip STM32F411RE
 probe-rs-cli download target/thumbv7em-none-eabihf/debug/app --chip STM32F411RE --base-address 0x08004000
