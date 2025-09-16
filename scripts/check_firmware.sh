256af64eeec810becdb7c66a16acb2ceda60beed
Md Mahbubur Rahman <mahbubur.r.aaman@gmail.com>
2025-09-16 07:13:12 +0600

    refactor comments for clarity

--- scripts/check_firmware.sh
+++ scripts/check_firmware.sh
@@ -6,5 +6,6 @@
 #           - Commercial license required for closed-source use
 # Author  : Md Mahbubur Rahman
 # URL     : <https://m-a-h-b-u-b.github.io>
-# GitHub  : <https://github.com/m-a-h-b-u-b/M2-Bootloader-RUST>
+# GitHub  : <https://github.com/m-a-h-b-u-b/M2-Bootloader-Rust>
+
 echo "Firmware valid"
